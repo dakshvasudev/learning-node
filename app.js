eababadd7284e4cf0544978ad1e19200cd0d0e69
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2024-01-21 12:18:51 +0530

    process exit

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+/// importing http
 const http = require("http");
 
+///creating a server using http, and sending a function that takes request and gives response
 const server = http.createServer((req, res) => {
   console.log(req);
+  ///exiting the process otherwise the loop will never end and the server will always be up and running
+  ///After this the code is completed.
+  process.exit();
 });
 
+///specifying the server port as 3000 where I need to listen.
 server.listen(3000);
