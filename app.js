b5db6cca47d50aa91169fdf5c4405625cf63f297
dakshvasudev <vasudevdaksh@gmail.com>
2024-01-22 22:18:55 +0530

    initial for section 3 assignment

--- app.js
+++
@@ -1,9 +0,0 @@
-/// importing http
-const http = require("http");
-const routes = require("./routes");
-
-///creating a server using http, and sending a function that takes request and gives response
-const server = http.createServer(routes);
-
-///specifying the server port as 3000 where I need to listen.
-server.listen(3000);
