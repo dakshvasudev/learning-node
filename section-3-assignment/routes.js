1544a1cebadd42936879f2e7cad3c325c84d3ec9
dakshvasudev <vasudevdaksh@gmail.com>
2024-01-22 22:22:41 +0530

    setting up inital server

---
+++ section-3-assignment/routes.js
@@ -0,0 +1,11 @@
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+  }
+  if (url === "/user") {
+  }
+  if (url === "/create-user") {
+  }
+};
