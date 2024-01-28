3002d0b88d2b5a586ccfe254bbf1a578e38378b9
dakshvasudev <vasudevdaksh@gmail.com>
2024-01-28 11:44:38 +0530

    assignment completion

--- section-5-assignment/app.js
+++ section-5-assignment/app.js
@@ -2,18 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the first middleware");
-  next();
+// app.use((req, res, next) => {
+//   console.log("in the first middleware");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("in the second middleware");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("in the third middleware");
+// });
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>This is the users page</h1>");
 });
 
-app.use((req, res, next) => {
-  console.log("in the second middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("in the third middleware");
+app.use("/", (req, res, next) => {
+  res.send("<h1>This is the default page</h1>");
 });
 
 app.listen(3000);
