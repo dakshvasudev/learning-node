8e730d326df9cee5d504a2a9b6e7e6edc5392d5f
dakshvasudev <vasudevdaksh@gmail.com>
2024-01-28 14:42:06 +0530

    handling routing in express

--- handling-req-express/app.js
+++ handling-req-express/app.js
@@ -1,23 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post("/add-product", (req, res, next) => {
-  res.send(
-    "<form action = '/product' method = 'POST'><input type = 'text' name = 'title'></input><button type = 'submit'>ADD PRODUCT</button></form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>This is the / page</h1>");
-});
+app.use(adminRoute);
+app.use(shopRoute);
 
 app.listen(3000);
---
+++ handling-req-express/routes/shop.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.use("/", (req, res, next) => {
+  res.send("<h1>This is the / page</h1>");
+});
+
+module.exports = router;
