42d7d9d23a5a9595da29fe28d39acba96b5fb607
dakshvasudev <vasudevdaksh@gmail.com>
2024-01-28 10:55:20 +0530

    Refactor app.js to use ExpressJS for server creation and listening

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -1,4 +1,3 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 
@@ -9,7 +8,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("In another middleware");
+  res.send("<h1>Hello from ExpressJS!</h1>");
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
