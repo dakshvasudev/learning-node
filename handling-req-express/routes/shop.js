acf80885b946b588b286a34633dee1232ace0cde
dakshvasudev <vasudevdaksh@gmail.com>
2024-01-30 02:44:58 +0530

    routing

--- handling-req-express/routes/shop.js
+++ handling-req-express/routes/shop.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send("<h1>This is the / page</h1>");
 });
 
