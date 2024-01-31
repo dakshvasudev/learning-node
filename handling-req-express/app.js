f3c83523b5f16f72874754c238f1e24020ebfba1
dakshvasudev <vasudevdaksh@gmail.com>
2024-02-01 01:16:47 +0530

    Add error handling for 404 page not found

--- handling-req-express/app.js
+++ handling-req-express/app.js
@@ -3,13 +3,14 @@ const bodyParser = require("body-parser");
 const adminRoute = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 const app = express();
+const path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoute);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
