e760fb3b5c50fb1b92113df164a2e3f2452d728d
dakshvasudev <vasudevdaksh@gmail.com>
2024-02-04 13:41:19 +0530

    users

--- section-5-assignment-2/app.js
+++ section-5-assignment-2/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const initialRoutes = require("./routes/initial");
+const usersRoutes = require("./routes/users");
 
+app.use(usersRoutes);
 app.use(initialRoutes);
 
 app.listen(3000);
