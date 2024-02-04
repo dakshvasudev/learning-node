50a5ecc24d353d30038f794c38b9dfc04723d618
dakshvasudev <vasudevdaksh@gmail.com>
2024-02-04 20:08:32 +0530

    integrating css using static

--- section-5-assignment-2/app.js
+++ section-5-assignment-2/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const initialRoutes = require("./routes/initial");
 const usersRoutes = require("./routes/users");
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "views", "css")));
 app.use(usersRoutes);
 app.use(initialRoutes);
 
